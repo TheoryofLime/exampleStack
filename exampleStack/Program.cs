6ddcd74de0af94b7f89f6e9eeb222479a1589686
user <user@PC>
2023-03-12 18:56:21 -0400

    Add project files.

---
+++ exampleStack/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exampleStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack <string> newStack= new Stack<string>();
+            // add items
+            newStack.Push("A");
+            newStack.Push("B");
+            newStack.Push("C");
+            newStack.Push("D");
+            newStack.Push("E");
+            // check for item and display
+            Console.WriteLine(newStack.Contains("C"));
+            // remove an item
+            newStack.Pop();
+            // count 
+            Console.WriteLine(newStack.Count);
+            // display all
+            foreach (string s in newStack)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
